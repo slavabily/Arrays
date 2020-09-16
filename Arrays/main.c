0425dac2850786dd1864a75a384e139ba05933ea
slava bily <slavabily@me.com>
2020-09-16 19:17:45 +0300

    Initial Commit

---
+++ Arrays/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Arrays
+//
+//  Created by slava bily on 16.09.2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
