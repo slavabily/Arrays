dbf880be5e3049eb1faa9b429dfccb0da7b185fa
slava bily <slavabily@me.com>
2020-09-21 11:13:04 +0300

    all functionality is completed

--- Arrays/main.c
+++ Arrays/main.c
@@ -9,6 +9,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+// MARK: fuctions for operating with array
+
+// printing
 void print_data(int d[], int size) {
     int i;
     for (i = 1; i <= size; i++) {
@@ -19,6 +22,7 @@ void print_data(int d[], int size) {
     }
 }
 
+// average calculation
 double average(int d[], int size) {
     int i;
     double avg = 0.0;
@@ -29,6 +33,7 @@ double average(int d[], int size) {
     return avg / size;
 }
 
+// max calculation
 int max_value(int d[], int size) {
     int i;
     
@@ -40,17 +45,18 @@ int max_value(int d[], int size) {
     return d[1];
 }
 
+
+//MARK: main routing
 int main(int argc, const char * argv[]) {
     FILE *ifp, *ofp;
     char number[20];
     char comment[50];
     int i, sz;
-    int data[10];
     
     ifp = fopen("arr", "r");
     ofp = fopen("answer-hw3", "w");
     
-    // MARK: Reading the first array element
+    // MARK: Reading the first element of the array
     fscanf(ifp, "%d", &sz);
     
     sprintf(number, "%d", sz);
@@ -68,14 +74,15 @@ int main(int argc, const char * argv[]) {
         fputs(number, ofp) ;
         fputs("\n", ofp) ;
     }
- // printing to the console
+    // printing to the console
     printf("%s %d\n",comment, sz);
     
     // MARK: Reading the array itself
+    int data[sz];
     for (i = 1; i <= sz; i++) {
         fscanf(ifp, "%d", &data[i]);
     }
-    // writing to the file
+    // writing to the output file
     strcpy(comment, "The array itself is: \t");
     if (number > 0 && comment > 0) {
         fputs(comment, ofp);
@@ -90,9 +97,36 @@ int main(int argc, const char * argv[]) {
     printf("%s", comment);
     print_data(data, sz);
     
-    printf("\n%g\n", average(data, sz));
+    // MARK: The average calculation
+    // writing to the output file
+    strcpy(comment, "The average of array elements is: \t");
+    sprintf(number, "%g", average(data, sz));
+    if (number > 0 && comment > 0) {
+        fputs(comment, ofp);
+        fputs(number, ofp) ;
+        fputs("\n", ofp) ;
+    }
+    // printing to the console
+    printf("\n%s", comment);
+    printf("%s", number);
     
-    printf("%d\n", max_value(data, sz));
+    // MARK: The max calculation
+    // writing to the output file
+    strcpy(comment, "The max of array elements is: \t");
+    sprintf(number, "%d", max_value(data, sz));
+    if (number > 0 && comment > 0) {
+        fputs(comment, ofp);
+        fputs(number, ofp) ;
+        fputs("\n", ofp) ;
+    }
+    // printing to the console
+    printf("\n%s", comment);
+    printf("%s\n", number);
+    
+    fclose(ofp);
+    fclose(ifp);
+    
+    printf("All results are printed into the output file 'answer-hw3'\n");
     
     return 0;
 }
